e1cf4a91d16dcb33a6058262e8c2bb9bb11d05e1
Zaqueu Alves De Oliveira Júnior <69696525+zaqueu-git@users.noreply.github.com>
2022-01-25 22:48:18 -0300

    Update and rename ExampleVO.php to exampleVO.php

---
+++ exampleVO.php
@@ -0,0 +1,12 @@
+<?php
+class ExampleVO
+{
+    public $zipCode;
+    public $street;
+    public $number;
+    public $complement;
+    public $district;
+    public $city;
+    public $state;
+    public $country;
+}
